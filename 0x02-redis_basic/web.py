9beda15cac09b58fbad5e1c69041e487fd5aa013
KalkM <106752167+KalkM@users.noreply.github.com>
2023-05-10 14:16:08 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,15 +1,32 @@
-"""Implements a get page function"""
+#!/usr/bin/env python3
+""" Redis Module """
+
+from functools import wraps
 import redis
 import requests
+from typing import Callable
+
+redis_ = redis.Redis()
 
 
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
+    @wraps(method)
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@count_requests
 def get_page(url: str) -> str:
-    """Tracks the amount of times a url is called"""
-    r = redis.Redis()
-    key = 'count:' + url
-    if not r.get(key):
-        r.setex('count:'+url, 10, 1)
-    else:
-        r.incr(key, 1)
-
-    return requests.get(url).content.decode()
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
