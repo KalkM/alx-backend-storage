9d4732cd21d320e48f6ca1f4a30cfe30f8fed68c
KalkM <106752167+KalkM@users.noreply.github.com>
2023-05-10 22:52:23 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,49 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
+''' In this tasks, we will implement a get_page function (prototype:
+def get_page(url: str) -> str:). The core of the function is very simple.
+It uses the requests module to obtain the HTML content of a particular
+URL and returns it.
+
+Start in a new file named web.py and do not reuse the code written in
+exercise.py.
+
+Inside get_page track how many times a particular URL was accessed in
+the key "count:{url}" and cache the result with an expiration time of
+10 seconds.
+
+Tip: Use http://slowwly.robertomurray.co.uk to simulate a slow response
+and test your caching.
+
+Bonus: implement this use case with decorators.
 '''
-import redis
 import requests
+import redis
 from functools import wraps
-from typing import Callable
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+db = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+def count_page_access(method):
+    ''' count number of time a website is accessed '''
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def func(url):
+        ''' wrap function '''
+        ckey = 'cached:' + url
+        cdata = db.get(ckey)
+        if cdata:
+            return cdata.decode('utf-8')
+
+        key = 'count:' + url
+        db.incr(key)
+        page = method(url)
+        db.set(ckey, page)
+        db.expire(ckey, 10)
+        return page
+    return func
 
 
-@data_cacher
+@count_page_access
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
+    ''' fetch content from url '''
     return requests.get(url).text
